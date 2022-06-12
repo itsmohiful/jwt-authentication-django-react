cc3b8a6cd6845c17b6de79e195e82748f020dc93
Soyad Mohiful Islam <mohiful.islam.ofc@gmail.com>
2022-06-13 00:03:53 +0600

    add serializers class and requirements.txt

--- jwt_app/api/urls.py
+++ jwt_app/api/urls.py
@@ -3,9 +3,10 @@ from rest_framework_simplejwt.views import (TokenObtainPairView,
                                             TokenRefreshView)
 
 from . import views
+from .views import MyTokenObtainPairView
 
 urlpatterns = [
     path('',views.get_routs),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
