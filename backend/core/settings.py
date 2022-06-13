341a5cfad886df2f5c24282bbb3fe0a5403227f7
Soyad Mohiful Islam <mohiful.islam.ofc@gmail.com>
2022-06-14 02:21:32 +0600

    add requirements.txt file

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-=6%5oqnb7&l!$wp)6m&c3ghqzreta2ye((cqw^t3kj^d+-z*7v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
