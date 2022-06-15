19ddac14a3eb136ef43e15fa0da9d8a5dcde40b5
Soyad Mohiful Islam <mohiful.islam.ofc@gmail.com>
2022-06-16 02:06:49 +0600

    path change for frontend folder

---
+++ frontend-&-backend/frontend/src/components/Header.js
@@ -0,0 +1,24 @@
+import React, {useContext} from 'react'
+import { Link } from 'react-router-dom'
+import AuthContext from '../context/AuthContext'
+
+const Header = () => {
+    let {user, logoutUser} = useContext(AuthContext)
+    return (
+        <div>
+            <Link to="/">Home</Link>
+            <span> | </span>
+
+            {user ? (
+                <p onClick={logoutUser}>LogOut</p>
+            ) : (
+                <Link to="/login">Login</Link>
+            )}
+            
+
+            {user && <p>Hello {user.username}</p>}
+        </div>
+    )
+}
+
+export default Header
---
+++ frontend-&-backend/frontend/src/context/AuthContext.js
@@ -0,0 +1,126 @@
+import { createContext, useState, useEffect } from 'react'
+import jwt_decode from "jwt-decode";
+import { useHistory } from 'react-router-dom'
+
+const AuthContext = createContext();
+export default AuthContext;
+
+
+export const AuthProvider = ({children}) =>{
+    
+    
+    let [authTokens, setAuthTokens] = useState(()=> localStorage.getItem('authTokens') ? JSON.parse(localStorage.getItem('authTokens')) : null)
+
+    let [user, setUser] = useState(()=> localStorage.getItem('authTokens') ? jwt_decode(localStorage.getItem('authTokens')) : null)
+
+    const history = useHistory()
+
+    let loginUser = async (e) => {
+        e.preventDefault()
+        console.log("form submitted")
+
+        let response = await fetch('http://127.0.0.1:8000/api/token/', {
+            mode:'no-cors',
+            method:'POST',
+
+            headers:{
+                'Content-Type' : 'application/json',
+                // 'charset': 'utf-8',
+                // "accept": "application/json",
+                // "Access-Control-Allow-Origin" : "*", 
+                // "Access-Control-Allow-Credentials" : true,
+            },
+
+            body:JSON.stringify({'username':e.target.username.value, 'password':e.target.password.value })
+        })
+
+        let data = await response.json()
+
+        // console.log("data : ", data)
+        //console.log("response : ", response)
+
+        if(response === 200){
+
+            setAuthTokens(data)
+            setUser(jwt_decode(data.access))
+            localStorage.setItem('authTokens', JSON.stringify(data))
+            history.push('/')
+
+        }else{
+            alert('something went wrong!')
+        }
+        
+        
+
+    }
+
+    let logoutUser = () =>{
+        setAuthTokens(null)
+        setUser(null)
+        localStorage.removeItem('authTokens')
+        history.push('/login')
+    }
+
+
+
+    let updateToken = async ()=> {
+
+        let response = await fetch('http://127.0.0.1:8000/api/token/refresh/', {
+            method:'POST',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify({'refresh':authTokens?.refresh})
+        })
+
+        let data = await response.json()
+        
+        if (response.status === 200){
+            setAuthTokens(data)
+            setUser(jwt_decode(data.access))
+            localStorage.setItem('authTokens', JSON.stringify(data))
+        }else{
+            logoutUser()
+        }
+
+        if(loading){
+            setLoading(false)
+        }
+    }
+
+
+
+    let contextData = {
+        user:user,
+        loginUser : loginUser,
+        logoutUser : logoutUser,
+    }
+
+
+    useEffect(()=> {
+
+        if(loading){
+            updateToken()
+        }
+
+        let fourMinutes = 1000 * 60 * 4
+
+        let interval =  setInterval(()=> {
+            if(authTokens){
+                updateToken()
+            }
+        }, fourMinutes)
+        return ()=> clearInterval(interval)
+
+    }, [authTokens, loading])
+
+
+
+    return (
+        <AuthContext.Provider value={contextData}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+
